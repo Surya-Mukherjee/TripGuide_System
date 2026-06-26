3d85daf8e94d7875778ba40459f9e1a1eee1df34
Surya-Mukherjee <suryamukherjee67@gmail.com>
2026-06-26 20:46:24 +0530

    Refactor review controller: rename updateGuideStats to updateGuideRating and update route integration

--- src/controllers/review.controller.js
+++ src/controllers/review.controller.js
@@ -5,35 +5,7 @@ import { User } from "../models/user.model.js";
 import { Guide } from "../models/guides.model.js";
 import { Review } from "../models/review.model.js";
 import mongoose from "mongoose";
-
-const addReview= asyncHandler(async(req,res)=>{
-    const guideId= req.params.guideId;
-    console.log("hello",guideId)
-    if(!guideId){
-        throw new apiError(404,"guide not found")
-    }
-   
-    const {rating,comment}=req.body
-  
-    const userId=req.user._id;
-    const userName=req.user.userName
-    
-     const review = await Review.create({
-         guideId,
-         userId,
-         userName,
-         comment,
-         rating
-     })
-     await updateGuideStats(guideId)
-     return res.json(
-        new apiResponse(
-            200,{},"review added successfully"
-        )
-     )
-})
-
- async function updateGuideStats(guideId){
+async function updateGuideRating(guideId){
   const stats= await Review.aggregate([
     {$match:{
         guideId:new mongoose.Types.ObjectId(guideId)
@@ -54,5 +26,33 @@ const addReview= asyncHandler(async(req,res)=>{
     }
   )
 }
+const addReview= asyncHandler(async(req,res)=>{
+    const guideId= req.params.guideId;
+    
+    if(!guideId){
+        throw new apiError(404,"guide not found")
+    }
+   
+    const {rating,comment}=req.body
+  
+    const userId=req.user._id;
+    const userName=req.user.userName
+    
+     const review = await Review.create({
+         guideId,
+         userId,
+         userName,
+         comment,
+         rating
+     })
+     await updateGuideRatings(guideId)
+     return res.json(
+        new apiResponse(
+            200,{},"review added successfully"
+        )
+     )
+})
 
-export {addReview,updateGuideStats}
+ 
+
+export {addReview,updateGuideRating}
--- src/routers/reviews.route.js
+++ src/routers/reviews.route.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyjwt } from "../middleware/auth.middleware.js";
-import { addReview, updateGuideStats } from "../controllers/review.controller.js";
+import { addReview } from "../controllers/review.controller.js";
 import { requireUser } from "../middleware/userCheck.middleware.js";
 import { reqbody } from "../middleware/bodycheck.middleware.js"
 import { upload } from "../middleware/multer.middleware.js";
@@ -8,7 +8,7 @@ const router= Router()
 
 
 router.route("/:guideId").post(upload.none(),reqbody,verifyjwt,requireUser,addReview)
-router.route("/").patch(verifyjwt,requireUser,updateGuideStats)
+router.route("/").patch(verifyjwt,requireUser,addReview)
 
 // router.route("/").delete(verifyjwt,requireUser,deleteReview);
 
